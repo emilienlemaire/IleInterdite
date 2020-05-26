9bf429d76dd575ee03119b6bda282e758027fa77
paul <p.patault@gmail.com>
2020-05-26 16:01:03 +0200

    Travail sur le Menu

--- src/com/egp/vues/StartVue.java
+++ src/com/egp/vues/StartVue.java
@@ -1,28 +1,55 @@
 package com.egp.vues;
 
 import com.egp.modeles.Modele;
+import javafx.geometry.Bounds;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.FlowPane;
-import javafx.scene.layout.Pane;
+import javafx.scene.image.Image;
+import javafx.scene.layout.*;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
 
 public class StartVue extends Scene {
     private final Pane root;
     private final Stage stage;
 
-    public StartVue(Pane root, Stage stage) {
+    public StartVue(Pane root, Stage stage) throws FileNotFoundException {
         super(root, 250, 300);
 
         this.root = root;
         this.stage = stage;
 
-        Button button = new Button("Passer au jeu");
+        Button jouerButton = new Button("Jouer");
+        jouerButton.setOnMouseClicked(mouseEvent -> setMainScene());
 
-        button.setOnMouseClicked(mouseEvent -> setMainScene());
+        Button paramButton = new Button("Paramètres");
 
-        root.getChildren().add(button);
+        Text title = new Text("Ile Interdite");
+
+        GridPane gridPane = new GridPane();
+        gridPane.setMinWidth(250);
+        gridPane.setMinHeight(300);
+        gridPane.setPadding(new Insets(30, 30, 30, 30));
+        gridPane.setVgap(15);
+        gridPane.setHgap(15);
+        gridPane.add(title,0,0);
+        gridPane.add(jouerButton,0,1);
+        gridPane.add(paramButton,0,2);
+
+        String path = "resources/background/background.png";
+        FileInputStream imageStream = new FileInputStream(path);
+        Image image = new Image(imageStream);
+        BackgroundImage backgroundImage = new BackgroundImage(image, BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT, BackgroundSize.DEFAULT);
+        //BackgroundFill myBF = new BackgroundFill(Color.BLUEVIOLET, new CornerRadii(1), new Insets(0.0,0.0,0.0,0.0));
+
+        gridPane.setBackground(new Background(backgroundImage));
+
+        root.getChildren().add(gridPane);
     }
 
     private void setMainScene() {
