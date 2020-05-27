17567ec51ffa37265e70358e64e5ea3bfb6486f9
Emilien Lemaire <emilien.lem@icloud.com>
2020-05-27 19:28:35 +0200

    Fini selection des joueurs

--- src/com/egp/vues/PlayerSelectVue.java
+++ src/com/egp/vues/PlayerSelectVue.java
@@ -13,35 +13,69 @@ import java.util.ArrayList;
 public class PlayerSelectVue extends VBox {
     private final ArrayList<PlayerType> playerTypes = new ArrayList<>();
     private final ArrayList<PlayerComboBoxVue> comboBoxVues = new ArrayList<>();
-    private final ArrayList<Button> plusButtons = new ArrayList<>();
+    private final ArrayList<HBox> hBoxes = new ArrayList<>();
+    private final Button plusButton = new Button("+");
+    private final Button minusButton = new Button("-");
 
     public PlayerSelectVue() {
         super();
+        plusButton.setOnMouseClicked(mouseEvent -> addPlayer());
+        minusButton.setOnMouseClicked(mouseEvent -> removePlayer());
+
         PlayerComboBoxVue comboBoxVue = new PlayerComboBoxVue();
         comboBoxVues.add(comboBoxVue);
-        playerTypes.add(PlayerType.Normal);
+        playerTypes.add(null);
         comboBoxVue.setOnAction(actionEvent -> {
             int idx = comboBoxVues.indexOf(comboBoxVue);
             playerTypes.set(idx, comboBoxVue.getValue());
         });
 
-        Button plusButton = new Button("+");
-        plusButton.setOnMouseClicked(mouseEvent -> addPlayer());
-
         HBox hBox = new HBox(comboBoxVue, plusButton);
-        hBox.setSpacing(10);
+        hBox.setSpacing(7.5);
+        hBoxes.add(hBox);
         this.setMaxWidth(250);
-        this.getChildren().addAll(hBox);
+        this.getChildren().addAll(hBoxes);
+    }
+
+    private void removePlayer() {
+        this.getChildren().clear();
+
+        hBoxes.remove(hBoxes.size() - 1);
+        hBoxes.get(hBoxes.size() - 1).getChildren().add(plusButton);
+        if (hBoxes.size() > 1) {
+            hBoxes.get(hBoxes.size() - 1).getChildren().add(minusButton);
+        }
+
+        comboBoxVues.remove(comboBoxVues.size() - 1);
+
+        playerTypes.remove(playerTypes.size() - 1);
+
+        this.getChildren().addAll(hBoxes);
     }
 
     public void addPlayer() {
         PlayerComboBoxVue comboBoxVue = new PlayerComboBoxVue();
         comboBoxVues.add(comboBoxVue);
-        playerTypes.add(PlayerType.Normal);
+
+        playerTypes.add(null);
         comboBoxVue.setOnAction(actionEvent -> {
             int idx = comboBoxVues.indexOf(comboBoxVue);
             playerTypes.set(idx, comboBoxVue.getValue());
         });
+        HBox hBox = new HBox(comboBoxVue, plusButton, minusButton);
+        hBox.setSpacing(7.5);
+        hBoxes.add(hBox);
         this.getChildren().clear();
+        this.getChildren().addAll(hBoxes);
+
+        if (comboBoxVues.size() > 4) {
+            for (HBox hBox1 : hBoxes) {
+                hBox1.getChildren().remove(plusButton);
+            }
+        }
+    }
+
+    public ArrayList<PlayerType> getPlayerTypes() {
+        return playerTypes;
     }
 }
--- src/com/egp/vues/StartVue.java
+++ src/com/egp/vues/StartVue.java
@@ -6,6 +6,7 @@ import com.egp.modeles.Modele;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.*;
@@ -27,9 +28,6 @@ public class StartVue extends Scene {
 
     public StartVue(Pane root, Stage stage){
         super(root, 250, 300);
-
-        this.players = new ArrayList<>();
-        this.players.add(PlayerType.Explorateur);
 
         try {
             this.getStylesheets().add(getURL("resources/styles/stylesheet.css").toExternalForm());
@@ -63,27 +61,11 @@ public class StartVue extends Scene {
             cols = Integer.parseInt(newValue);
         });
 
-        TextField playerField = new TextField();
-        playerField.setText("1");
-        playerField.setMaxWidth(40);
-        playerField.textProperty().addListener((observable, oldValue, newValue) -> {
-            if (!newValue.matches("\\d*")) {
-                playerField.setText(newValue.replaceAll("[^\\d]", ""));
-            }
-            int nbPlayer = Integer.parseInt(newValue);
-            this.players.clear();
-            for(int i = 0; i<nbPlayer; i++)
-                this.players.add(PlayerType.Explorateur);
-        });
 
         Text sizeText = new Text("Taille de la grille: ");
         HBox grilleSizeBox = new HBox();
         grilleSizeBox.setAlignment(Pos.CENTER_LEFT);
         grilleSizeBox.getChildren().addAll(rowField, new Text("X"), colField);
-
-        HBox playerNumberBox = new HBox();
-        playerNumberBox.setAlignment(Pos.CENTER_LEFT);
-        playerNumberBox.getChildren().addAll(new Text("Nombre de joueurs: "), playerField);
 
         Text title = new Text("Ile Interdite");
 
@@ -92,6 +74,10 @@ public class StartVue extends Scene {
         GridPane grilleSizeBoxCont = new GridPane();
         grilleSizeBoxCont.add(sizeText,0,0);
         grilleSizeBoxCont.add(grilleSizeBox,0,1);
+
+
+        PlayerSelectVue playerSelectVue = new PlayerSelectVue();
+        this.players = playerSelectVue.getPlayerTypes();
 
         GridPane gridPane = new GridPane();
         gridPane.setAlignment(Pos.CENTER_LEFT);
@@ -104,9 +90,8 @@ public class StartVue extends Scene {
 
         gridPane.add(title,0,0);
         gridPane.add(grilleSizeBoxCont,0,1);
-        gridPane.add(playerNumberBox, 0, 2);
-        gridPane.add(jouerButton,0,4);
-        gridPane.add(new PlayerSelectVue(), 0, 3);
+        gridPane.add(playerSelectVue, 0, 2);
+        gridPane.add(jouerButton,0,3);
 
         gridPane.setBackground(new Background(backgroundImage));
 
@@ -114,13 +99,34 @@ public class StartVue extends Scene {
     }
 
     private void setMainScene() {
-        Modele modele = new Modele(cols, rows, players, 1./3.);
-        root.getChildren().clear();
+        boolean allPlayerSelect = true;
 
-        FlowPane mainRoot = new FlowPane(this.root);
-        MainVue mainVue = new MainVue(modele, mainRoot, this.stage);
+        for (PlayerType playerType :
+                players) {
+            if (playerType == null) {
+                allPlayerSelect = false;
+                break;
+            }
+        }
 
-        this.stage.setScene(mainVue);
+        if (allPlayerSelect){
+            Modele modele = new Modele(cols, rows, players, 1./3.);
+            root.getChildren().clear();
+
+            FlowPane mainRoot = new FlowPane(this.root);
+            MainVue mainVue = new MainVue(modele, mainRoot, this.stage);
+
+            this.stage.setScene(mainVue);
+        } else {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+
+            alert.setTitle("Selectionnez tous les types de joueurs");
+            alert.setContentText("Il semblerait que vous ayez oublié de selectionner un ou plusieurs type de" +
+                    "joueurs, veuillez tous les selectionner ou supprimer ceux aue vous ne voulez pas.");
+
+            alert.showAndWait();
+        }
+
     }
 
 }
